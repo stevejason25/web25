98fb3007edde7188681aea9dee400c0a22da4f87
jason <201909851@est.umss.edu>
2025-12-11 09:15:43 -0400

    arreglo de la version node.js 24 a node.js 20

--- server.js
+++ server.js
@@ -10,29 +10,28 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-// --- TUS RUTAS ---
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
 app.use('/api/questions', require('./routes/questionRoutes'));
 
 const PORT = process.env.PORT || 3000;
 
-// Configuración de Certificados (Igual que antes)
 const options = {
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
 };
 
-// 2. Creamos el servidor usando SPDY (HTTP/2)
+// 5. Crear el servidor HTTP/2
 spdy.createServer(options, app).listen(PORT, (error) => {
     if (error) {
-        console.error(error);
+        console.error('Error al iniciar el servidor:', error);
         return process.exit(1);
     } else {
         console.log(`🚀 Servidor HTTP/2 seguro corriendo en el puerto ${PORT}`);
-        console.log(`🍃 MongoDB conectado`);
+        console.log(`🍃 MongoDB conectado exitosamente`);
     }
 });
